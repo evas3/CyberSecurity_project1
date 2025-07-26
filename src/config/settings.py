568916736bb5816c89fed99c822da58092cfdc79
Eva-Maria <evamaria.asplund@gmail.com>
2025-07-26 16:34:08 +0300

    csrf

--- src/config/settings.py
+++ src/config/settings.py
@@ -46,12 +46,13 @@ INSTALLED_APPS = [
 	'src.pages'
 ]
 
+# For flaw 1 csrf token needs to be checked
 MIDDLEWARE = [
     'axes.middleware.AxesMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
